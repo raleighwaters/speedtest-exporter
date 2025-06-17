b18c4763230cadafd42bbd900518a8de0142cb9f
raleighwaters <RaleighWaters@gmail.com>
2025-06-16 21:44:20 -0400

    Renamed speedtest.py to executor.py

--- speedtest/main.py
+++ speedtest/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Response
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
-from speedtest.speedtest import start_background_speedtest
+from speedtest.executor import start_background_speedtest
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
